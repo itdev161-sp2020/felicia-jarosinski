2f282621ce120f873cce331ced013bb2c2645cee
Felicia J <jarosinf@gmatc.matc.edu>
2020-03-21 22:01:32 -0500

    Changes to server.js and package.json to troubleshoot get and post API

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import connectDatabase from './config/db';
+
+//initialize express application
+
+const app = express();
+
+//connect database
+connectDatabase();
+
+//configure Middleware
+app.use(express.json({ extended: false }));
+
+// API endpoints
+/**
+ * @route GET
+ * @desc Test endpoint
+ */
+
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+    );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+**/   
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+//connection listener
+app.listen(3000, () => console.log ('Express server running on port 3000'));
