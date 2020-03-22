09a313da92f24d4259ab0aa9911e5680c949f548
Felicia J <jarosinf@gmatc.matc.edu>
2020-03-22 16:20:20 -0500

    server.js commented out lines 15-19 to troubleshoot local host connection

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ connectDatabase();
 
 //configure Middleware
 app.use(express.json({extended: false}));
-app.use(
-    cors({
-        origin:'http://localhost:3000'
-    })
-);
+//*app.use(
+// cors({
+ //       origin:'http://localhost:3000'
+ //   })
+//);
 // API endpoints
 /**
  * @route GET
@@ -31,10 +31,27 @@ app.get('/', (req, res) =>
  * @route POST api/user
  * @desc Register user
 **/   
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post('/api/users',
+    [
+        check ('name', 'Please enter your name')
+        .not()
+        .isEmpty(), 
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+            ),isLength({ min: 6 })
+        ],
+        (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).json({ errors.array() });
+            } else {
+                return res.send(req.body);
+            }
+        }
+    
+);
 
 //connection listener
 const port = 3000;
