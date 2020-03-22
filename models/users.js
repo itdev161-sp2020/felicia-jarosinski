a0e07d29a94bc04c1a3722aa0afaeb0c9d00f208
Felicia J <jarosinf@gmatc.matc.edu>
2020-03-22 13:18:37 -0500

    further troubleshooting get and post API with package.json and server.js

--- models/users.js
+++ models/users.js
@@ -16,6 +16,6 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('user', UserSchema);
+const user = mongoose.model('user', UserSchema);
 
-export default User;
+export default user;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult} from 'express-validator';
+import cors from 'cors';
 
 //initialize express application
 
@@ -9,8 +11,12 @@ const app = express();
 connectDatabase();
 
 //configure Middleware
-app.use(express.json({ extended: false }));
-
+app.use(express.json({extended: false}));
+app.use(
+    cors({
+        origin:'http://localhost:3000'
+    })
+);
 // API endpoints
 /**
  * @route GET
@@ -22,7 +28,7 @@ app.get('/', (req, res) =>
     );
 
 /**
- * @route POST api/users
+ * @route POST api/user
  * @desc Register user
 **/   
 app.post('/api/users', (req, res) => {
@@ -31,4 +37,7 @@ app.post('/api/users', (req, res) => {
 });
 
 //connection listener
-app.listen(3000, () => console.log ('Express server running on port 3000'));
+const port = 3000;
+app.listen(port, () => 
+console.log ('Express server running on port 3000')
+);
