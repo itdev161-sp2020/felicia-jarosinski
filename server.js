3b560620957664ac200462b8ac6a8d1d96f46300
Felicia J <jarosinf@gmatc.matc.edu>
2020-05-15 14:33:27 -0500

    created authorize user api endpoint

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
+import auth from './middleware/auth';
 //initialize express application
 
 const app = express();
@@ -92,6 +93,18 @@ app.post('/api/users',
             }
         }   
 );
+/**
+ * @route GET api/auth
+ * @desc Authenticate user
+ */
+app.get('/api/auth', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Unknown server error');
+    }
+});
 
 //connection listener
 const port = 5000;
