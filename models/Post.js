21c2c388c27b201f3573639d3b72936ba40eedab
Felicia J <jarosinf@gmatc.matc.edu>
2020-05-21 00:25:08 -0500

    created a post model

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: 'ObjectId',
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('post', PostSchema);
